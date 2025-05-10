eba66c1071758ffcc19ba1a94da0d9faea136f1e
ubadineke <ubadinekeprince@gmail.com>
2025-05-10 15:59:51 +0100

    added metadata pointer

--- programs/rwa-token-standard/src/instructions/create_asset.rs
+++ programs/rwa-token-standard/src/instructions/create_asset.rs
@@ -1,16 +1,17 @@
 use crate::constants::ASSET;
 use crate::states::*;
 use anchor_lang::prelude::*;
-use anchor_spl::associated_token::Create;
-use anchor_spl::metadata::Metadata;
+use anchor_lang::system_program::{create_account, CreateAccount};
+
 use anchor_spl::{
-    associated_token::AssociatedToken,
-    token_2022::spl_token_2022::extension::{
-        interest_bearing_mint::InterestBearingConfig, ExtensionType,
+    token_2022::{
+        initialize_mint2,
+        spl_token_2022::{extension::ExtensionType, pod::PodMint},
+        InitializeMint2,
     },
     token_interface::{
-        get_mint_extension_data, token_metadata_initialize, Mint, Token2022,
-        TokenMetadataInitialize,
+        metadata_pointer_initialize, token_metadata_initialize,
+        MetadataPointerInitialize, Mint, Token2022, TokenMetadataInitialize,
     },
 };
 
@@ -43,9 +44,60 @@ impl CreateAsset<'_> {
         let mint_auth_signer_seeds: &[&[&[u8]]] =
             &[&[ASSET.as_bytes(), &mint_key.as_ref(), &[ctx.bumps.asset]]];
 
-        let asset = &mut ctx.accounts.asset;
+        // Calculate space required for mint and extension data
+        let mint_size =
+            ExtensionType::try_calculate_account_len::<PodMint>(&[ExtensionType::MetadataPointer])?;
+
+        // Calculate minimum lamports required for size of mint account with extensions
+        let lamports = (Rent::get()?).minimum_balance(mint_size);
+
+        // Invoke System Program to create new account with space for mint and extension data
+        create_account(
+            CpiContext::new(
+                ctx.accounts.system_program.to_account_info(),
+                CreateAccount {
+                    from: ctx.accounts.authority.to_account_info(),
+                    to: ctx.accounts.mint.to_account_info(),
+                },
+            ),
+            lamports,
+            mint_size as u64,
+            &ctx.accounts.token_program.key(),
+        )?;
+
+        // Initialize the MetadataPointer extension
+        // This instruction must come before the instruction to initialize the mint data
+        metadata_pointer_initialize(
+            CpiContext::new(
+                ctx.accounts.token_program.to_account_info(),
+                MetadataPointerInitialize {
+                    token_program_id: ctx.accounts.token_program.to_account_info(),
+                    mint: ctx.accounts.mint.to_account_info(),
+                },
+            ),
+            Some(ctx.accounts.authority.key()),
+            Some(mint.key()),
+        )?;
+
+        initialize_mint2(
+            CpiContext::new(
+                ctx.accounts.token_program.to_account_info(),
+                InitializeMint2 {
+                    mint: ctx.accounts.mint.to_account_info(),
+                },
+            ),
+            6,
+            &ctx.accounts.authority.key(),
+            Some(&ctx.accounts.authority.key()),
+        )?;
+
         ctx.accounts
             .initialize_token_metadata(&params, mint_auth_signer_seeds)?;
+
+        let asset = &mut ctx.accounts.asset;
+        asset.authority = ctx.accounts.authority.key();
+        asset.mint = ctx.accounts.mint.key();
+        asset.status = true;
 
         emit!(AssetMetadataEvent {
             mint: ctx.accounts.mint.key().to_string(),
